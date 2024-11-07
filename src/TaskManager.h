75f0ad6cb4a903d2eaf6a9e5c0895c82b0aa5bd4
theHDM <157935022+theHDM@users.noreply.github.com>
2024-11-07 12:23:19 -0500

    Update TaskManager.h

--- src/TaskManager.h
+++ src/TaskManager.h
@@ -5,8 +5,6 @@
 // these should run in the background
 // set an alarm to run the "on_irq" boolean every "tick" microseconds.
 // load in the background functions you want using the "bind" language.
-//
-
 #include <iostream>
 #include <vector>
 #include <functional>
@@ -24,18 +22,17 @@ class task_mgr_obj {
                 exec_on_trigger = arg_func;
             }
             void increment(int add_uS) {
-                std::cout << " incrementing " << counter << " by " << add_uS;
+                std::cout << " incr " << counter << "+" << add_uS;
                 counter += add_uS;
-                std::cout << " now it's " << counter;
+                std::cout << "=" << counter;
             }
             void execute() {
                 exec_on_trigger();
             }
             bool triggered() {
-                std::cout << " checking trigger ";
-                std::cout << " counter is " << counter << " period is " << period << "; ";
+                std::cout << " chk " << counter << ".vs." << period << "; ";
                 if (counter >= period) {
-                    counter -= period;
+                    counter = counter % period;
                     return true;
                 }
                 return false;
@@ -43,10 +40,10 @@ class task_mgr_obj {
         };
         int tick_uS = 0;
     public:
-        std::vector<task_obj> task_list;
-        void set_tick_uS(int arg_uS) {
+        task_mgr_obj(int arg_uS) {
             tick_uS = arg_uS;
         }
+        std::vector<task_obj> task_list;
         int get_tick_uS() {
             return tick_uS;
         }
@@ -69,22 +66,49 @@ class A {
             return repeat_uS;
         }
         void doWork() {
-            std::cout << " A:didWork. ";
+            std::cout << " A:synth. ";
+        }
+};
+class B {
+    private:
+        int repeat_uS = 1;
+    public:
+        void set_repeat(int arg_uS) {
+            repeat_uS = arg_uS;
+        }
+        int get_repeat() {
+            return repeat_uS;
+        }
+        void doWork() {
+            std::cout << " B:rotary. ";
+        }
+};
+class C {
+    private:
+        int repeat_uS = 1;
+    public:
+        void set_repeat(int arg_uS) {
+            repeat_uS = arg_uS;
+        }
+        int get_repeat() {
+            return repeat_uS;
+        }
+        void doWork() {
+            std::cout << " C:hexes. ";
         }
 };
 
-task_mgr_obj task_mgr;
+
+task_mgr_obj task_mgr(8);
 
 static bool on_irq() {
     std::cout << "in IRQ";
     int t = task_mgr.get_tick_uS();
     for (auto &i : task_mgr.task_list) {
-        std::cout << ", incremented a task by " << t << "; ";
         i.increment(t);
     }
     for (auto &i : task_mgr.task_list) {
         if (i.triggered()) {
-            std::cout << "triggered a task; ";
             i.execute();
             return true;
         }
@@ -93,19 +117,30 @@ static bool on_irq() {
 }
 
 int main() {
-    task_mgr.set_tick_uS(24);
     A routine_A;
-    routine_A.set_repeat(512);
+    routine_A.set_repeat(32);
     task_mgr.add_task(
         routine_A.get_repeat(), 
         std::bind(&A::doWork, &routine_A)
     );
+    B routine_B;
+    routine_B.set_repeat(256);
+    task_mgr.add_task(
+        routine_B.get_repeat(), 
+        std::bind(&B::doWork, &routine_B)
+    );
+    C routine_C;
+    routine_C.set_repeat(16);
+    task_mgr.add_task(
+        routine_C.get_repeat(), 
+        std::bind(&C::doWork, &routine_C)
+    );
     int i = 0;
-    for (int j = 0; j < 100; j++) {
+    for (int j = 0; j < 72; j++) {
         if (on_irq()) {
             i++;
         }
-        std::cout << "loop#" << j << " triggers=" << i << "\n";
+        std::cout << i << "/" << j  << "\n";
     }
     return i;
 }
