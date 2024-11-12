b7274699d82dc352ef29cc124d7519acc1f42c2c
theHDM <157935022+theHDM@users.noreply.github.com>
2024-11-12 12:23:41 -0500

    Update timing.h

--- src/timing.h
+++ src/timing.h
@@ -2,15 +2,15 @@
 #include "utils.h"
 #include "hardware/timer.h"
 
-timeStamp getTheCurrentTime() {
-  timeStamp temp = timer_hw->timerawh;
+time_uS getTheCurrentTime() {
+  time_uS temp = timer_hw->timerawh;
   return (temp << 32) | timer_hw->timerawl;
 }
 
 class softTimer {
   private:
-    timeStamp startTime;
-    timeStamp delay_uS;
+    time_uS startTime;
+    time_uS delay_uS;
     bool running;
     bool finishNow;
   public:
@@ -20,7 +20,7 @@ class softTimer {
       running = false;
       finishNow = false;
     };
-    void start(timeStamp _delay_uS, timeStamp _defer_uS) {
+    void start(time_uS _delay_uS, time_uS _defer_uS) {
       startTime = getTheCurrentTime() + _defer_uS;
       delay_uS = _delay_uS;
       running = true;
@@ -58,16 +58,16 @@ class softTimer {
       }
       return false;
     }    
-    timeStamp getStartTime() {
+    time_uS getStartTime() {
       return startTime;  
     }
-    timeStamp getElapsed() {
-      timeStamp temp = getTheCurrentTime();
+    time_uS getElapsed() {
+      time_uS temp = getTheCurrentTime();
       return (temp < startTime ? 0 : temp - startTime);
     }
-    timeStamp getRemaining() {
+    time_uS getRemaining() {
       if (running) {
-        timeStamp temp = getElapsed();
+        time_uS temp = getElapsed();
         if (finishNow || (temp >= delay_uS)) {
           return 0;
         } else {
@@ -77,7 +77,7 @@ class softTimer {
         return 0;
       }  
     }
-    timeStamp getDelay()  {
+    time_uS getDelay()  {
       return delay_uS;
     }
 };
