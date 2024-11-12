f565c676f7d4cf713b556638ad7ce100e6d59e2d
theHDM <157935022+theHDM@users.noreply.github.com>
2024-11-12 10:33:02 -0500

    Update synth.h

--- src/synth.h
+++ src/synth.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "syntacticSugar.h"
+#include "utils.h"
 
-const uint8_t polyphony_limit = 8;
+const int polyphony_limit = 8;
 /*
   The hybrid synth sound blends between
   square, saw, and triangle waveforms
@@ -13,36 +13,44 @@ const float transition_saw_low = 440.0;
 const float transition_saw_high = 880.0;
 const float transition_triangle = 1760.0;
 
-enum {
+enum { // different instrument generator rules
   simple_lookup = 0,
   generator_square = 1,
   generator_saw = 2,
   generator_triangle = 3,
   generator_hybrid = 4
 };
-
+// an instrument is a definition of how to
+// synthesize a sound. it contains
+// 1. either a generator rule (i.e. square / saw wave)
+// or an audio waveform loop
+// 2. an ADSR envelope
+// 3. some basic operational functions.
 struct instrument_t {
-  uint8_t  type = generator_square;
-  uint8_t  samples[256];
-  uint     attack  = 0;    // in samples
-  uint     decay   = 0;    // in samples
-  uint8_t  sustain = 255;  // level [0..255]
-  uint     release = 0;    // in samples
-  void set_attack(uint ms) {
+  int  type = generator_square;
+  int  samples[256];
+  int  attack = 0;    // in samples
+  int  decay = 0;    // in samples
+  int  sustain = 255;  // level [0..255]
+  int  release = 0;    // in samples
+  void set_attack(int ms) {
     attack = ms * sampleRate / 1000;
   }
-  void set_decay(uint ms) {
+  void set_decay(int ms) {
     decay = ms * sampleRate / 1000;
   }
-  void set_release(uint ms) {
+  void set_release(int ms) {
     release = ms * sampleRate / 1000;
   }
-  void set_waveform(uint8_t wave_table[256]) {
+  void set_waveform(int wave_table[256]) {
     type = simple_lookup;
-    for (uint8_t i = 0; i < 256; i++) {
+    for (int i = 0; i < 256; i++) {
       samples[i] = wave_table[i];
     }
   }  
+  // consider a match if the parameters are the same
+  // but ignore the treatment of the sample table
+  // for now until i figure it out
   bool operator==(const instrument_t &rhs) const {
     return this->type    == rhs.type    &&
         // this->samples == rhs.samples &&
@@ -52,30 +60,35 @@ struct instrument_t {
            this->release == rhs.release;
   }
 };
-
-/* 
-  an oscillator object returns
-  the position of a synthesized
-  waveform based on a counter
-  that is incremented based on
-  a target frequency. works
-  with 8-bit loops of 256 samples.
-*/
+// an oscillator object returns
+// the position of a synthesized
+// waveform based on a counter
+// that is incremented based on
+// a target frequency. it uses
+// loops of 256 8-bit samples [-127 to 127]. *** actually still 0 to 255 so change this soon
+// the waveform is generated based
+// on the definition of its associated
+// instrument_t object (type of sound
+// and its ADSR envelope).
 class oscillator_t {
 private:
   instrument_t _instrument;
-  uint8_t  _stored_wave_form[256];
-  bool     _playing = false;
-  bool     _noteOn = false;
-  float    _freq = 0;
-  uint8_t  _modulation = 0;   // must be [0..127]
-  uint8_t  _velocity = 0;     // must be [0..127]
-  bool     _applyEQ = true;
-  uint8_t  _eq = 8;
-  uint16_t _incr = 0;  // inverse of frequency
-  uint16_t _ctr = 0;   // waveform lookup counter
-  uint64_t _smp = 0;   // samples since onset
-  void     hybridParameters(uint8_t &refToShape, uint8_t &refToMod) {
+  int _stored_wave_form[256];
+  bool _playing = false;
+  bool _noteOn = false;
+  float _freq = 0;
+  int _modulation = 0;   // must be [0..127]
+  int _velocity = 0;     // must be [0..127]
+  bool _applyEQ = true;
+  int _eq = 8;
+  int _incr = 0;  // inverse of frequency
+  int _ctr = 0;   // waveform lookup counter
+  int _smp = 0;   // samples since onset
+  void hybridParameters(int &refToShape, int &refToMod) {
+    // at low frequencies play a square wave
+    // at middle frequencies play a saw wave
+    // at high frequencies play a triangle wave
+    // in between boundaries, create in-between shapes
     if (_freq < transition_square) {
       refToShape = generator_square;
       refToMod = 0;
@@ -97,72 +110,82 @@ private:
       }
     }
   }
-  void     regenerateWaveForm() {
-    uint8_t shape = _instrument.type;
-    uint8_t mod = _modulation;
+  void regenerateWaveForm() {
+    // whenever this is called, build the waveform based on
+    // the generator or wavetable lookup for this channel.
+    // this usually happens if the mod wheel, underlying
+    // frequency, or user option change could affect the wave shape
+    int shape = _instrument.type;
+    int mod = _modulation;
     if (shape == generator_hybrid) {
       hybridParameters(shape, mod);
     }
-    switch (shape) {
-    case generator_square:
-      for (uint8_t i = 0; i < (128 + mod); i++) {
+    switch (shape) { // generator functions to make wave tables
+    case generator_square: // the mod wheel changes the duty cycle
+      for (int i = 0; i < (128 + mod); i++) {
         _stored_wave_form[i] = 0;
       }
-      for (uint8_t i = (128 + mod); i < 256; i++) {
+      for (int i = (128 + mod); i < 256; i++) {
         _stored_wave_form[i] = 255;
       }
       break;    
-    case generator_saw:
-      for (uint8_t i = 0; i < mod; i++) {
+    case generator_saw: // the mod wheel changes the slope of the saw
+	    // i.e. more mod makes the saw "squared off"
+      for (int i = 0; i < mod; i++) {
         _stored_wave_form[i] = 0;
       }
-      for (uint8_t i = mod; i < (128 + mod); i++) {
+      for (int i = mod; i < (128 + mod); i++) {
         _stored_wave_form[i] = lerp(0,255,mod,128+mod,i);
       }
-      for (uint8_t i = (128 + mod); i < 256; i++) {
+      for (int i = (128 + mod); i < 256; i++) {
         _stored_wave_form[i] = 255;
       }
       break;    
-    case generator_triangle:
-      for (uint8_t i = 0; i < (128 + mod); i++) {
+    case generator_triangle: // the mod wheel moves the apex of the wave
+	    // i.e. more mod makes the triangle more "sawtooth-like"
+      for (int i = 0; i < (128 + mod); i++) {
         _stored_wave_form[i] = lerp(0,255,0,128+mod,i);
       }
-      for (uint8_t i = (128 + mod); i < 256; i++) {
+      for (int i = (128 + mod); i < 256; i++) {
         _stored_wave_form[i] = lerp(0,255,256,128+mod,i);
       }
       break;
-    default:
-      for (uint8_t i = 0; i < 256; i++) {
+    default: // otherwise grab the existing wavetable
+      for (int i = 0; i < 256; i++) {
         _stored_wave_form[i] = _instrument.samples[i];
       }
       break;
     }
   }
-  void     recalculateEQ() {
+  void recalculateEQ() {
     // EQ   8    7    6    5    4    3    2    1    0
     // dB  +4   +3   +1.5  0   -2   -4.5 -8   -14  -inf
     // dB = 20*ln(EQ)/ln(10).
-         if (_freq <     8.0) {_eq = 0;} // -inf low pass
-    else if (_freq <    60.0) {_eq = 8;} // +4dB rumble
-    else if (_freq <   100.0) {_eq = 8;} // +4dB punch
-    else if (_freq <   450.0) {_eq = 8;} // +4dB boom
-    else if (_freq <  1000.0) {_eq = 8;} // +4dB honk
-    else if (_freq <  1500.0) {_eq = 7;} // +3dB tin
-    else if (_freq <  2000.0) {_eq = 6;} // +1.5 tin
-    else if (_freq <  2500.0) {_eq = 5;} //  0dB crunch
-    else if (_freq <  3000.0) {_eq = 4;} // -2dB crunch
-    else if (_freq <  3500.0) {_eq = 3;} // -4.5 pierce
-    else if (_freq <  4000.0) {_eq = 4;} // -2dB pierce
-    else if (_freq <  4500.0) {_eq = 5;} //  0dB presence
-    else if (_freq <  5000.0) {_eq = 6;} // +1.5 presence
-    else if (_freq < 12500.0) {_eq = 7;} // +3dB sib
+    // NOTE this is NOT real EQ, it's adjusting the volume
+    // only based on the note frequency, not its harmonics
+    //                                       EQ  MIDI notes
+         if (_freq <     8.0) {_eq = 0;} // -inf below  0 (C -1)
+    else if (_freq <    60.0) {_eq = 8;} // +4dB 0 to 34 (Bb1)
+    else if (_freq <  1500.0) {_eq = 7;} // +3dB   to 90 (F#6)
+    else if (_freq <  2000.0) {_eq = 6;} // +1.5   to 95 (B 6)
+    else if (_freq <  2500.0) {_eq = 5;} //  0dB   to 99 (Eb7)
+    else if (_freq <  3000.0) {_eq = 4;} // -2dB  to 102 (F#7)
+    else if (_freq <  3500.0) {_eq = 3;} // -4.5  to 104 (Ab7)
+    else if (_freq <  4000.0) {_eq = 4;} // -2dB  to 107 (B 7)
+    else if (_freq <  4500.0) {_eq = 5;} //  0dB  to 109 (C#8)
+    else if (_freq <  5000.0) {_eq = 6;} // +1.5  to 111 (Eb8)
+    else if (_freq < 13290.0) {_eq = 7;} // +3dB  to 127.99 (G#9-1c)
     else                      {_eq = 0;}
   }
 public:
-  bool isPlaying() {
+  bool isPlaying() { 
     return _playing;
   }
-  void noteOn() {
+  // calling noteOn / Off is a passive event
+  // it sets the flags so that when next_sample()
+  // is next called, this channel will be marked
+  // as able to begin playing a note.
+  void noteOn() { 
     _noteOn = true;
     _playing = true;
     _smp = 0;
@@ -178,6 +201,10 @@ public:
   void set_frequency(float f, float sampleIncr) {
     if (f != _freq) {
       _freq = f;
+     // _incr drives the speed of the wave table
+     // lookup. it might be better to simply
+     // use floats all around instead of force
+     // to 16-bit integer, but for now keep it this way
       _incr = round(f * sampleIncr);
       if (_instrument.type == generator_hybrid) {
         regenerateWaveForm();
@@ -187,12 +214,12 @@ public:
       }
     }
   }
-  bool can_be_modulated(uint8_t _generator) {
+  bool can_be_modulated(int _generator) {
     return ((_generator == generator_triangle) 
          || (_generator == generator_square) 
          || (_generator == generator_saw));
   }
-  void set_mod(uint8_t lvl) {
+  void set_mod(int lvl) {
     if (lvl != _modulation) {
       _modulation = lvl;
       if (can_be_modulated(_instrument.type)) {
@@ -200,7 +227,7 @@ public:
       }
     }
   }
-  void set_vel(uint8_t lvl) {
+  void set_vel(int lvl) {
     _velocity = lvl;
   }
   void set_instrument(instrument_t instr) {
@@ -209,84 +236,87 @@ public:
       regenerateWaveForm();
     }
   }
-  uint8_t nextSample() { 
+  int nextSample() { 
     if (!(_playing)) {
       return 0;
     }
     _ctr += _incr;
     _smp++;
-    uint8_t wave_raw = _stored_wave_form[(_ctr & 0xFF00) >> 8];
-    uint8_t envelope = 0;
+    int wave_raw = _stored_wave_form[(_ctr & 0xFF00) >> 8];
+    int envelope = 0;
     if (_noteOn) {
+      // apply attack decay sustain
       if (_smp >= (_instrument.attack + _instrument.decay)) {
         envelope = _instrument.sustain;
       } else if (_smp < _instrument.attack) {
-        envelope = lerp((uint8_t)1,(uint8_t)255,0,_instrument.attack,_smp);          
+        envelope = lerp((int)1,(int)255,0,_instrument.attack,_smp);          
       } else {
-        envelope = lerp((uint8_t)255,_instrument.sustain,0,_instrument.decay,_smp-_instrument.attack);
+        envelope = lerp((int)255,_instrument.sustain,0,_instrument.decay,_smp-_instrument.attack);
       }
     } else if (_smp < _instrument.release) {
-        envelope = lerp(_instrument.sustain,(uint8_t)0,0,_instrument.release,_smp);        
+      // apply release
+      envelope = lerp(_instrument.sustain,(int)0,0,_instrument.release,_smp);        
     }
-		if (!(envelope)) {
-			_playing = false;
-			return 0;
-		}
+    if (!(envelope)) {
+      // if past the release length
+      _playing = false;
+      return 0;
+    }
     return (wave_raw * envelope * _eq * _velocity) >> 18;
     // 26 bits: 8 (waveform) + 8 (envelope) + 3 (eq) + 7 (velocity)
     // downsample to 8 bits = shift 18 to the right
   }
 };
-
-/*
-  a synth is a collection of
-  oscillators whose states are
-  "mixed" to produce a stream
-  of raw audio output
-  consider using signed int -127 to 128 instead
-	so that stuff is centered.
-
-*/
+// a synth is a collection of
+// oscillators whose states are
+// "mixed" to produce a stream
+// of raw audio output.
 class synth_t {
 private:
-	uint 	_sample_rate;
-	float	_sample_increment;
-  std::queue<uint8_t> _channelQueue;
+  int _sample_rate;
+  float _sample_increment;
+  int_queue _channelQueue;
 public:
+  // sort of like MIDI, we want fixed channels
+  // and to access via index, which we do via
+  // the channel queue
   oscillator_t channel[polyphony_limit];
-  void setup(uint sample_rate) {    
+  void setup(int sample_rate) {
+    // unlikely that the sample rate will change mid-execution.
     _sample_rate = sample_rate;
-		_sample_increment = 65536.0 / _sample_rate;
-		for (uint8_t i = 0; i < polyphony_limit; i++) {
+    // every instrument in the synth will use this value
+    // as the basis for reading samples.
+    // i suppose this could be stored as a float and then
+    // push the floating math back to the synth.
+    // because it's getting rounded anyway?
+    // think it over.
+    _sample_increment = 65536.0 / _sample_rate;
+    // initialize the list of open polyphony channels
+    for (int i = 0; i < polyphony_limit; i++) {
       _channelQueue.push(i + 1);
     }
   }
-  void set_modulation(uint8_t lvl) {
-    for (uint8_t i = 0; i < polyphony_limit; i++) {
+  void set_modulation(int lvl) {
+    for (int i = 0; i < polyphony_limit; i++) {
       channel[i].set_mod(lvl);
     }
   }
-  uint8_t kompressor(uint16_t original) {
+  int kompressor(int original) { // this probably doesn't work
+    // i think we're better off using power law to attenuate the
+    // output based on number of voices playing simultaneously
+    // scale output = 1.0 / sqrt(# voices). it works nicely
+    // with base output = 64, see v1.0 of firmware LEGACYsynth.h
+    // meanwhile keep this here for now.
          if (original >= 2040) {return 255;}
     else if (original >=  512) {return 160 + (original >> 4);}
     else if (original >=  256) {return 128 + (original >> 3);}
     else                       {return       (original >> 1);}
   }
-  uint8_t writeNextSample() {
-    uint16_t mix = 0;
-    uint8_t poly = 0;
-    for (auto c : channel) {
-      mix += c.nextSample();  // 8 bits each
-      poly += c.isPlaying();
-    }
-    return kompressor(mix); // compress ~ 3:1, normalize, and downsample
-  }
-  uint8_t tryNoteOn(instrument_t instr, float freq, uint8_t vel, uint8_t mod) {
+  int tryNoteOn(instrument_t instr, float freq, int vel, int mod) {
     if (_channelQueue.empty()) {
-      return 0;
+      return 0; // the calling function should know that channel zero means nothing was played.
     }
-    uint8_t ch = _channelQueue.front() - 1;
-    _channelQueue.pop();
+    int ch = pop_front(_channelQueue) - 1;
     channel[ch].set_instrument(instr);
     channel[ch].set_frequency(freq, _sample_increment);
     channel[ch].set_vel(vel);
@@ -294,23 +324,35 @@ public:
     channel[ch].noteOn();
     return ch + 1;
   }
-  void tryNoteOff(uint8_t ch) {
+  void tryNoteOff(int ch) {
     _channelQueue.push(ch);
     channel[ch - 1].noteOff();
+    // probably not even necessary to call it "try", it just "do"es.
   }
-  uint8_t count_voices_playing() {
-    uint8_t temp = 0;
-    for (uint8_t i = 0; i < polyphony_limit; i++) {
+  int count_voices_playing() {
+    int temp = 0;
+    for (int i = 0; i < polyphony_limit; i++) {
       temp += channel[i].isPlaying();
     }
     return temp;
   }
+  int writeNextSample() {
+    int mix = 0;
+    int poly = 0;
+    for (auto c : channel) {
+      // either here, or in the synth::nextSample,
+      // need to change to signed int expression.
+      mix += c.nextSample();  // 8 bits each
+      poly += c.isPlaying();
+    }
+    return kompressor(mix); // deprecate? compress ~ 3:1, normalize, and downsample
+  }
 };
 synth_t synth;
 // -------------------
-// here are some instrument presets
+// here are some instrument presets -- TODO: EXPRESS AS +/- 0..127
 // -------------------
-const uint8_t sine[256] = {
+const int sine[256] = {
     0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   2,   3,   3, 
     4,   5,   6,   7,   8,   9,  10,  12,  13,  15,  16,  18,  19,  21,  23,  25, 
    27,  29,  31,  33,  35,  37,  39,  42,  44,  46,  49,  51,  54,  56,  59,  62, 
@@ -328,7 +370,7 @@ const uint8_t sine[256] = {
    49,  46,  44,  42,  39,  37,  35,  33,  31,  29,  27,  25,  23,  21,  19,  18, 
    16,  15,  13,  12,  10,   9,   8,   7,   6,   5,   4,   3,   3,   2,   1,   1
 };
-const uint8_t strings[256] = {
+const int strings[256] = {
     0,   0,   0,   1,   3,   6,  10,  14,  20,  26,  33,  41,  50,  59,  68,  77, 
    87,  97, 106, 115, 124, 132, 140, 146, 152, 157, 161, 164, 166, 167, 167, 167, 
   165, 163, 160, 157, 153, 149, 144, 140, 135, 130, 126, 122, 118, 114, 111, 109, 
@@ -346,7 +388,7 @@ const uint8_t strings[256] = {
    58,  56,  54,  52,  50,  49,  48,  47,  46,  45,  45,  44,  43,  42,  41,  40, 
    39,  37,  35,  33,  31,  28,  25,  22,  19,  16,  13,  10,   7,   5,   2,   1
 };
-const uint8_t clarinet[256] = {
+const int clarinet[256] = {
     0,   0,   2,   7,  14,  21,  30,  38,  47,  54,  61,  66,  70,  72,  73,  74, 
    73,  73,  72,  71,  70,  71,  72,  74,  76,  80,  84,  88,  93,  97, 101, 105, 
   109, 111, 113, 114, 114, 114, 113, 112, 111, 110, 109, 109, 109, 110, 112, 114, 
