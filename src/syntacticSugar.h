ef51f70ef48097fe6a68bed47e7eaf0ea1335c20
theHDM <157935022+theHDM@users.noreply.github.com>
2024-11-11 21:02:03 -0500

    Update syntacticSugar.h

--- src/syntacticSugar.h
+++ src/syntacticSugar.h
@@ -17,12 +17,13 @@ void resize_matrix(int_matrix& m, size_t sizeA, size_t sizeB) {
 using func = std::function<void()>;
 
 #include <deque>
-#include <queue>                // standard C++ library construction to store open channels in microtonal mode (use "std::queue" to invoke it)
+#include <queue> // make sure that all queue objects are removed and changed to d.e.queue.
 using time_queue = std::deque<unsigned long long int>;
 using int_queue = std::deque<int>;
 
 #include <string>               // standard C++ library string classes (use "std::string" to invoke it); these do not cause the memory corruption that Arduino::String does.
 using str = std::string;
+
 
 enum {
   btn_off     = 0b00,
@@ -31,23 +32,13 @@ enum {
   btn_hold    = 0b11
 };
 
-/*
-  C++ returns a negative value for 
-  negative N % D. This function
-  guarantees the mod value is always
-  positive.
-*/
+// function that guarantees the mod value is always positive.
 int positiveMod(int n, int d) {
   return (((n % d) + d) % d);
 }
-/*
-  There may already exist linear interpolation
-  functions in the standard library. This one is helpful
-  because it will do the weighting division for you.
-  It only works on byte values since it's intended
-  to blend color values together. A better C++
-  coder may be able to allow automatic type casting here.
-*/
+// extensions of linear interpolation used in this project
+
+// eventually replace this one with template lerp
 byte byteLerp(byte xOne, byte xTwo, float yOne, float yTwo, float y) {
   float weight = (y - yOne) / (yTwo - yOne);
   int temp = xOne + ((xTwo - xOne) * weight);
@@ -69,6 +60,24 @@ T lerp(T xOne, T xTwo, float yOne, float yTwo, float y) {
     return (xOne * (1 - w)) + (xTwo * w);
   }
 }
+
+#include <map>
+using lerp_map_t = std::map<float, int>;
+using lerp_inverse_map_t = std::map<int, float>;
+lerp_inverse_map_t invert(lerp_map_t lerp_map) {
+  lerp_inverse_map_t temp;
+  for (auto i : lerp_map) {
+    temp[i->second] = i->first;
+  }
+  return temp;
+}
+template <typename T_key, typename T_value>
+T_value lerp_over_map(const std::map<T_key, T_value>& lerp_map, T_key pos) {
+  auto LB = lerp_map.lower_bound(pos);
+  auto UB = std::next(LB);
+  return lerp(LB->second, UB->second, LB->first, UB->first, pos);
+}
+
 /*
   it would be nice to have a wrapper for the queue function
   to return the front of the queue and pop it, because
