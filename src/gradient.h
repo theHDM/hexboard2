77da8ac92e5bb2e7203ea05d4600323c516caddf
Nicholas Fox <157935022+theHDM@users.noreply.github.com>
2024-11-12 21:22:51 -0500

    Update gradient.h

--- src/gradient.h
+++ src/gradient.h
@@ -14,27 +14,28 @@ enum class generate_gradient {
 // be recalled based on a given stop position (from 0 to 1)
 struct gradient_t {
 	std::map<float, pixel_code> pixel_code_map;
-	// pixel_code is the neoPixel integer format
 	pixel_code lookup(float pos) {
 		return pixel_code_map.lower_bound(pos);
 	}
+
 	size_t resolution; // number of steps minus one... i.e. [0..1>
 	// more steps = smoother gradient, but longer to calculate
-	
 	generate_gradient method;
-	// shade_tint options
-		// HSV_color_type base_hsv;
-		// RGB_color_type base_rgb;
-		// OKLCH_color_type base_oklch;
+	// shade_tint base color inputs; -1 values if "untouched"
+	HSV_color base_hsv = {-1,-1,-1};
+	sRGB_color base_rgb = {-1,-1,-1};
+	okLCH_color base_oklch = {-1,-1,-1};
 	// define_stops options
-		// std::map<float, HSV_color_type> stops_hsv;
-		// std::map<float, RGB_color_type> stops_rgb;
-		// std::map<float, OKLCH_color_type> stops_oklch;
+	std::map<float, HSV_color> stops_hsv = {{-1,-1}};
+	std::map<float, sRGB_color> stops_rgb = {{-1,-1}};
+	std::map<float, okLCH_color> stops_oklch = {{-1,-1}};
+	std::map<float, okLAB_color> stops_converted;
 	// rainbow options HSL, HSV, HCL
-		// float constant_saturation;
-		// float constant_chroma;
-		// float constant_value;
-		// float constant_lightness;
+	float constant_saturation = -1;
+	float constant_chroma = -1;
+	float constant_value = -1;
+	float constant_lightness = -1;
+	
 	void generate_rainbow(int const_sat, int const_val) {
 		// generate a rainbow of hues
 		// direct calculation; no interpolation needed
@@ -43,41 +44,68 @@ struct gradient_t {
 	}
 	// kite options similar to rainbow i think.
 	
-	// use the rgb/hsv color type, not the colorCode
-	void generate_gradient(convert to other kinds);
-	void generate_gradient(std::map<float, color_t> gradient_map) {
+	void generate_gradient() {
 		// interpolate gradient colors and convert
 		// to neopixel format ahead of time
 		// overloaded lerp to blend colors perceptually
-		// then you can do
-
-		// convert gradient map into OKLAB
 		for (float i = 0; i < 1; i += 1/(number_of_steps - 1)) {
-			
-			color_code_map[i] = linterp(convert_to_gradient_map, i).as_neoPixel;
+			pixel_code_map[i] = convert_to_neoPixel(
+				lerp_over_map(stops_converted, i)
+			);
 		}
 	}
-	// use the rgb/hsv color type, not the colorCode
-	void generate_shades_of(/* HSV base_color*/) {
-		// TBD
-		std::map<float, color_t> shade;
-		shade[0] = /* black */;
-		shade[0.5] = base_color;
-		shade[1] = /* white */;
-		generate_gradient(shade);
+	void generate_defined_stops() {
+		// convert any entered stops into okLAB
+		for (auto i : stops_hsv) {
+			if (i->first >= 0 && i->first <= 1) {
+				stops_converted[i->first] = convert_to_okLAB(i->second);
+			}
+		}
+		for (auto i : stops_rgb) {
+			if (i->first >= 0 && i->first <= 1) {
+				stops_converted[i->first] = convert_to_okLAB(i->second);
+			}
+		}
+		for (auto i : stops_oklch) {
+			if (i->first >= 0 && i->first <= 1) {
+				stops_converted[i->first] = convert_to_okLAB(i->second);
+			}
+		}
+		generate_gradient();
+	}
+	void generate_shade_tint() {
+		stops_converted[0] = {0, 0, 0}; // black
+		stops_converted[0] = {1, 0, 0}; // white
+		if (base_oklch.l >= 0) {
+			stops_converted[0.5] = convert_to_okLAB(base_oklch);
+		} else if (base_hsv.v >= 0) {
+			stops_converted[0.5] = convert_to_okLAB(base_hsv);
+		} else if (base_rgb.r >= 0) {
+			stops_converted[0.5] = convert_to_okLAB(base_rgb);
+		}
+		generate_gradient();
 	}
 }
 
-struct reactive_coloring_t {
-	gradient_t gradient;        //color lookup table
-	int* ptr_to_reactive_value; //i.e. timer, key pressure, wheel
-	std::map<int,float> rule_table; // e.g. -1 = 0.0  0 = 0.5  127 = 1.00
-	colorCode get_color() {
-		return gradient.lookup(
-			lerp_over_map(
-				rule_table, *ptr_to_reactive_value
-			)
-		);
+// this defines a reactive value.
+// 1. a pointer to an integer to "react" to
+// 2. a map from values that integer could be, to reaction positions [0..1]
+// at any point you can call pos() and it should real-time update.
+struct reactive_position_t {
+	int* ptr_to_reactive_value; // i.e. timer, key pressure, wheel
+	std::map<int,float> rule_table; // e.g. -1 = 0.0  0 = 0.5  127 = 1.0
+	float pos = lerp_over_map(rule_table, *ptr_to_reactive_value);
+}
+
+// this defines a "coloring rule", and contains
+// 1. a gradient map of colors
+// 2. a reactive position on the map
+// call "get_color" to lookup the color that should be shown
+//
+struct coloring_rule_t {
+	gradient_t gradient;        // color lookup table
+	reactive_position_t react;
+	pixel_code get_color() {
+		return gradient.lookup(react.pos());
 	}
-	
 }
